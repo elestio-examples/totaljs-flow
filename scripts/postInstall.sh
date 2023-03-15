7799f4462e8fa5a2d83732c822f0f0d8d4ec38e5
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-03-15 08:33:34 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,13 +5,16 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for software to be ready ..."
 sleep 10s;
 
-target=$(docker-compose port flow 8000)
+# url=$(curl -i https://${DOMAIN})
+#     echo $url > url.txt
 
-curl http://${target}/fapi/password/ \
-  -H 'accept: text/plain, */*; q=0.01' \
-  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
-  -H 'content-type: application/json; charset=UTF-8' \
-  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36' \
-  -H 'x-requested-with: XMLHttpRequest' \
-  --data-raw '{"login":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'","passwordreply":"'${ADMIN_PASSWORD}'"}' \
-  --compressed
+# target=$(docker-compose port flow 8000)
+
+# curl http://${target}/fapi/password/ \
+#   -H 'accept: text/plain, */*; q=0.01' \
+#   -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+#   -H 'content-type: application/json; charset=UTF-8' \
+#   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36' \
+#   -H 'x-requested-with: XMLHttpRequest' \
+#   --data-raw '{"login":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'","passwordreply":"'${ADMIN_PASSWORD}'"}' \
+#   --compressed
